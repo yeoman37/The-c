4b54f46faeded313f56a80f038a35ee0bb6b0c8a
yeoman37 <rxyhp@qq.com>
2018-06-18 23:49:50 +0800

    p42

---
+++ C Programming A Modern Approach/p42.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	
+int i,j;
+float x,y;
+
+i = 10;
+j = 20;
+x = 43.2892;
+y = 5527.0;
+
+printf("i = %d, j = %d, x = %f, y = %f\n", i,j,x,y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
